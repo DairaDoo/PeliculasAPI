058651aa2297dbe6da4dbebc4d0d947866b4b11b
Dairan De Jesús <dairandemora.work@gmail.com>
2025-04-25 21:12:50 -0400

    Add project files.

---
+++ PeliculasAPI/RepositorioEnMemoria.cs
@@ -0,0 +1,23 @@
+﻿using PeliculasAPI.Entidades;
+
+namespace PeliculasAPI
+{
+    public class RepositorioEnMemoria
+    {
+        private List<Genero> _generos;
+
+        public RepositorioEnMemoria()
+        {
+            _generos = new List<Genero>
+            {
+                new Genero { Id = 1, Nombre = "Comedia" },
+                new Genero { Id = 1, Nombre = "Comedia" }
+            };
+        }
+
+        public List<Genero> ObtenerTodosLosGeneros()
+        {
+            return _generos;
+        }
+    }
+}
