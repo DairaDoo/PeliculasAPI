6070b18acc9c93fdc7f4d09d14513dec7c3ab270
DairanDoo <your_email@example.com>
2025-04-25 21:48:31 -0400

    Rutas creadas en el controlador.

---
+++ PeliculasAPI/Controllers/GenerosController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PeliculasAPI.Entidades;
+
+namespace PeliculasAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class GeneroController
+    {
+
+        [HttpGet]
+        public List<Genero> Get()
+        {
+            var repositorio = new RepositorioEnMemoria();
+            var generos = repositorio.ObtenerTodosLosGeneros();
+
+            return generos;
+        }
+
+        [HttpGet]
+        public Genero? Get(int id)
+        {
+            var repositorio = new RepositorioEnMemoria();
+            var genero = repositorio.ObtenerPorId(id);
+            return genero;
+        }
+
+    }
+}
