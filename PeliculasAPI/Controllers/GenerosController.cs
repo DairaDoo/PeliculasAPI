278d5f161f5943f6c41d245836dfe46f7e27792b
Dairan De Jesús <dairandemora.work@gmail.com>
2025-04-30 14:50:39 -0400

    Agregando rutas, viendo como en una misma ruta puede cambiar la petición basandote en el tipo de dato del argumento, y como varias rutas pueden dirigirse al mismo endpoint.

--- PeliculasAPI/Controllers/GenerosController.cs
+++ PeliculasAPI/Controllers/GenerosController.cs
@@ -4,24 +4,52 @@ using PeliculasAPI.Entidades;
 namespace PeliculasAPI.Controllers
 {
     [Route("api/[controller]")]
-    public class GeneroController
+    public class GenerosController
     {
 
         [HttpGet]
+        [HttpGet("listado")]
+        [HttpGet("/listadoGeneros")]
         public List<Genero> Get()
         {
-            var repositorio = new RepositorioEnMemoria();
+            var repositorio = new RepositorioEnMemoria();   
             var generos = repositorio.ObtenerTodosLosGeneros();
 
             return generos;
         }
 
-        [HttpGet]
+        [HttpGet("{id:int}")] // api/generos/500
         public Genero? Get(int id)
         {
             var repositorio = new RepositorioEnMemoria();
             var genero = repositorio.ObtenerPorId(id);
             return genero;
+        }
+
+        [HttpGet("{nombre}")] // api/generos/Felipe
+        public Genero? Get(string nombre)
+        {   
+            var repositorio = new RepositorioEnMemoria();
+            var genero = repositorio.ObtenerPorId(1);
+            return genero;
+        }
+
+        [HttpPost]
+        public void Post()
+        {
+
+        }
+
+        [HttpPut]
+        public void Put()
+        {
+
+        }
+
+        [HttpDelete]
+        public void Delete()
+        {
+
         }
 
     }
