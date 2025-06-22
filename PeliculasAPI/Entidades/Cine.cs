700e2bda969a9b27cdeedf1894930c5e1210d286
DairanDoo <your_email@example.com>
2025-06-22 18:29:46 -0400

    Creando entidad Cine, donde añadimos un valor con tipo de dato Point, que representa la ubicación de los cines, lo implementamos usando la liberia EFNetTopologySuite

---
+++ PeliculasAPI/Entidades/Cine.cs
@@ -0,0 +1,16 @@
+﻿using NetTopologySuite.Geometries;
+using System.ComponentModel.DataAnnotations;
+
+namespace PeliculasAPI.Entidades
+{
+    public class Cine
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(75)]
+        public required string Nombre { get; set; }
+        // Tipo de Dato Point que se utiliza con la Libreria Net TopologySuite para manejar datos geográficos.
+        public required Point Ubicacion { get; set; }
+    }
+}
