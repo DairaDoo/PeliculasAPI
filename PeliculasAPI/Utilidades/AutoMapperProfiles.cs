f3a04e8a0914a44906efd896a122e354cce4003f
Dairan De Jesús <dairandemora.work@gmail.com>
2025-06-25 17:10:38 -0400

    Creando ruta en el controlador que devuelve nombre información relevate a mostrar sobre el actor al frontend utilizando un DTO (PeliculaActorDTO), para mostrar la data ya  creada desde el frontend sobre películas y actores.

--- PeliculasAPI/Utilidades/AutoMapperProfiles.cs
+++ PeliculasAPI/Utilidades/AutoMapperProfiles.cs
@@ -47,6 +47,8 @@ namespace PeliculasAPI.Utilidades
             CreateMap<ActorCreacionDTO, Actor>()
                 .ForMember(x => x.Foto, opciones => opciones.Ignore());
             CreateMap<Actor, ActorDTO>();
+
+            CreateMap<Actor, PeliculaActorDTO>();
         }
 
         private void ConfigurarMapeoGeneros()
