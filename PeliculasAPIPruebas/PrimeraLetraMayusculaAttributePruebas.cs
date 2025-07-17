d4e49803970bd49f238c5b6d85f5d76566fa26ad
DairanDoo <your_email@example.com>
2025-07-17 15:11:42 -0400

    Ejecutando una primera prueba unitaria en la función o metodo PrimeraLetraMayusculaAttribute

---
+++ PeliculasAPIPruebas/PrimeraLetraMayusculaAttributePruebas.cs
@@ -0,0 +1,29 @@
+﻿using PeliculasAPI.Validaciones;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeliculasAPIPruebas
+{
+    [TestClass]
+    public sealed class PrimeraLetraMayusculaAttributePruebas
+    {
+        [TestMethod]
+        public void IsValid_DebeRetornarExitoso_SiElValorEsVacio()
+        {
+            // Preparar
+            var primeraLetraMayusculaAttribute = new PrimeraLetraMayusculaAttribute();
+            var validationContext = new ValidationContext(new object());
+            var valor = string.Empty;
+
+            // Probar
+            var resultado = primeraLetraMayusculaAttribute.GetValidationResult(valor, validationContext);
+
+            // Verificar
+            Assert.AreEqual(expected: ValidationResult.Success, actual: resultado);
+        }
+    }
+}
